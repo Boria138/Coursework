f9edbd9ce0df2b39164e837cf55dbee9908a5080
Boria138 <boriabloger@protonmail.com>
2024-03-01 12:14:24 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const accordion = document.getElementsByClassName("accordion_header");
+
+for(i = 0; i<accordion.length; i++){
+  accordion[i].addEventListener("click", function(e){
+    let CurrentElement = e.target.closest(".accordion_header");
+    let CurrentnContent = CurrentElement.nextElementSibling;
+    CurrentElement.classList.toggle("active");
+    if(CurrentElement.classList.contains("active")){
+      CurrentnContent.style.maxHeight = CurrentnContent.scrollHeight + "px";
+    }else{
+      CurrentnContent.style.maxHeight = 0;
+    }
+  })
+}
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "menu") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
